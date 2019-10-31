f858c5bdd59a2f52064659d54c0c34d1cb4db566
Yash Panchal <yashvardan@Dark-Empire.local>
2019-10-30 21:34:28 -0400

    feat: added score counter and player switches after an event

--- app.js
+++ app.js
@@ -13,18 +13,44 @@ let scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
   // 1. Random Number
   let dice = Math.floor(Math.random() * 6) + 1;
+
   // 2. Display the result
   let diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
-  // 3. Update the round score if the rolled number wasn't 1.
   diceDOM.src = 'dice-' + dice + '.png';
+
+  // 3. Update the round score if the rolled number wasn't 1.
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    // Next Player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+
+
+  }
+  
 
 
 
